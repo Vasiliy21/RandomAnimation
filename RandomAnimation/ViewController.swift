bc50589ee0e8ae77ad55391f704f46d5d298b631
Vasiliy Startsev <Vasiliystartsev@gmail.com>
2022-08-06 23:43:48 +0300

    debug

--- RandomAnimation/ViewController.swift
+++ RandomAnimation/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var animationLabel: UILabel!
+    @IBOutlet var animationView: SpringView!
+    @IBOutlet var animationButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animationView.layer.cornerRadius = 10
+        animationButton.layer.cornerRadius = 10
+    }
+
+    @IBAction func animationButtonPressed(_ sender: SpringButton) {
+    
+        animationView.animation = "\(getRandomPreset())"
+        animationView.curve = "\(getRandomCurve())"
+        animationView.animate()
+
+        animationLabel.text = " preset: \(animationView.animation)\n curve: \(animationView.curve)\n forse: \(animationView.force)\n duration: \(animationView.duration)\n delay: \(animationView.delay)"
+
+    }
+}
+extension ViewController {
+    func getRandomPreset() -> String {
+        var presetAnimations: [String] = []
+        for animation in AnimationPreset.allCases {
+            presetAnimations.append(animation.rawValue)
+        }
+        let preset = presetAnimations.randomElement() ?? ""
+        return preset
+    }
+
+    func getRandomCurve() -> String {
+        var curveAnimations: [String] = []
+        for curve in AnimationCurve.allCases {
+            curveAnimations.append(curve.rawValue)
+        }
+        let curve = curveAnimations.randomElement() ?? ""
+        return curve
     }
 
 
 }
-
